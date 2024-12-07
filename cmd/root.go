2fc034e5f2b4d06e3244a4ccbab285f44af43fee
David Lin <davidlin9812@gmail.com>
2024-12-06 20:42:20 -0500

    Add basic list view for tui

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"heatcold/internal/leetcode"
 	tui2 "heatcold/internal/tui"
 	"os"
 
@@ -17,6 +19,14 @@ func init() {
 var problemStore *store.ProblemStore
 var tui bool
 
+type item struct {
+	title, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
 var rootCmd = &cobra.Command{
 	Use:   "yeetcode",
 	Short: "Track your leetcode progress",
@@ -30,7 +40,14 @@ var rootCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		if tui {
-			p := tea.NewProgram(tui2.InitialModel(), tea.WithAltScreen())
+			info, err := leetcode.GetQuestionInformation(1)
+			if err != nil {
+				fmt.Println("Invalid")
+				os.Exit(1)
+			}
+			m := tui2.Model{List: list.New([]list.Item{item{title: info["name"], desc: info["difficulty"]}}, list.NewDefaultDelegate(), 0, 0)}
+			m.List.Title = "List of Questions"
+			p := tea.NewProgram(m, tea.WithAltScreen())
 			if _, err := p.Run(); err != nil {
 				os.Exit(1)
 			}
--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -1,10 +1,15 @@
 package tui
 
 import (
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
 type Model struct {
+	List list.Model
 }
 
 func InitialModel() Model {
@@ -22,11 +27,16 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.List.SetSize(msg.Width-h, msg.Height-v)
 	}
 
-	return m, nil
+	var cmd tea.Cmd
+	m.List, cmd = m.List.Update(msg)
+	return m, cmd
 }
 
 func (m Model) View() string {
-	return ""
+	return docStyle.Render(m.List.View())
 }
